22e6f62e43f2f3409eed98b83299fc0df430ace5
Danny-Smith3 <101818745+Danny-Smith3@users.noreply.github.com>
2022-03-31 08:56:34 -0700

    Bomb Map2 Changes
    forgot to upload these with previous commit

---
+++ Timer.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour {
+
+    public TMP_Text timerText;
+    private float timeBeforeDetonate;
+    private float secondsPastMinute;
+    private bool bombEnabled = true;
+    public GameObject explode;
+    private bool won = true;
+
+    // Start is called before the first frame update
+    void Start() {
+        timeBeforeDetonate = 120f;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (bombEnabled) {
+            if (timeBeforeDetonate < 0f) {
+                won = false;
+                ParticleSystem[] particles = explode.GetComponentsInChildren<ParticleSystem>();
+                GameObject bomb = GameObject.Find("Bomb");
+                AudioSource bombSound = bomb.GetComponent<AudioSource>();
+                bombSound.Play();
+                foreach (ParticleSystem particle in particles) {
+                    Debug.Log("explode");
+                    particle.Play();
+                }
+                bombDisable();
+            }
+            timeBeforeDetonate -= Time.deltaTime;
+            if (timeBeforeDetonate < 60f) {
+                timerText.text = "Bomb Timer: 0:" + Mathf.Round(timeBeforeDetonate).ToString();
+            } else if (timeBeforeDetonate < 120f) {
+                secondsPastMinute = timeBeforeDetonate - 60f;
+                timerText.text = "Bomb Timer: 1:" + Mathf.Round(secondsPastMinute).ToString();
+            } else {
+                secondsPastMinute = timeBeforeDetonate - 120f;
+                timerText.text = "Bomb Timer: 2:" + Mathf.Round(secondsPastMinute).ToString();
+            }
+        }
+    }
+
+    public void bombDisable() {
+        bombEnabled = false;
+        GameObject player = GameObject.Find("Player");
+        PlayerHealth health = player.GetComponent<PlayerHealth>();
+        health.didWin = won;
+        health.startEndGame();
+    }
+}
